55026aca36302fffed3d43cb2f69ea97688bc36d
yosi475 <yosi475@naver.com>
2026-07-19 10:41:57 +0900

    [Passed] 물의 상태 (142ms, 10MB)

---
+++ trail0/물의 상태/state-of-water.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n < 0) {
+            System.out.print("ice");
+        }else if (n >= 100) {
+            System.out.print("vapor");
+        }else{
+            System.out.print("water");
+        }
+        
+    }    
+}
