c19c898040fb53d689243e04cd8422e2dcfb97cc
yosi475 <yosi475@naver.com>
2026-07-19 10:59:24 +0900

    [Passed] 차례로 출력 (90ms, 9MB)

---
+++ trail0/차례로 출력/print-in-order.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int n = 5; n < 18; n++) {
+            System.out.print(n + " ");// Please write your code here.
+        }
+    }
+}
