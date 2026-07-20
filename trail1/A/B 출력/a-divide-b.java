3b252281ad25b7124e50776d6078b63156cbc317
yosi475 <yosi475@naver.com>
2026-07-20 16:35:55 +0900

    [Passed] A/B 출력 (139ms, 11MB)

---
+++ trail1/A/B 출력/a-divide-b.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a / b + ".");
+        a = a % b;
+        for(int i = 0; i<20; i++) {
+            a *= 10;
+            System.out.print(a / b);
+            a %= b;
+        }
+        
+    }
+}
