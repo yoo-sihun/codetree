c3600efded96fb2a877a34d65533e4a93b7b6703
yosi475 <yosi475@naver.com>
2026-07-20 15:32:19 +0900

    [Passed] 다양하게 출력 (102ms, 9MB)

---
+++ trail1/다양하게 출력/print-in-variety.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Total days in Year");
+        System.out.print(365);
+        System.out.print("\n");
+        System.out.println("Circumference rate");
+        System.out.println(3.1415926535);// Please write your code here.
+    }
+}
