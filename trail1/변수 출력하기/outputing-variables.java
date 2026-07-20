0f4495889f1585cd0d83e3c6bfecc62dc284c405
yosi475 <yosi475@naver.com>
2026-07-20 15:41:04 +0900

    [Passed] 변수 출력하기 (110ms, 9MB)

---
+++ trail1/변수 출력하기/outputing-variables.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        char b = 'C';
+        System.out.printf("%d...%s", a, b);// Please write your code here.
+    }
+}
