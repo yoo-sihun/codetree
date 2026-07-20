8ceea1d88afaacc77f18ca5f46c9c5d8c02cb38b
yosi475 <yosi475@naver.com>
2026-07-20 16:18:00 +0900

    [Passed] N번 더하기 (137ms, 10MB)

---
+++ trail1/N번 더하기/add-n-times.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            System.out.println(a = a + n);
+        }
+    }
+}
