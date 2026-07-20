70cc3103b04c66e941627711c9d816b2f48806fc
yosi475 <yosi475@naver.com>
2026-07-20 16:08:34 +0900

    [Passed] 홀수만 출력 (145ms, 10MB)

---
+++ trail1/홀수만 출력/output-only-odd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        for(int i = A; i <= B; i+=2){
+            System.out.print(i + " ");
+        }
+        // Please write your code here.
+    }
+}
