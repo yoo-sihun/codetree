d597e17de157624e222145e414cdf09b2ca4794b
yosi475 <yosi475@naver.com>
2026-07-23 16:35:01 +0900

    [Passed] 문자열 두번 출력하기 (123ms, 10MB)

---
+++ trail1/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        for(int i = 0; i < 2; i++){
+            System.out.println(input);
+        }// Please write your code here.
+    }
+}
